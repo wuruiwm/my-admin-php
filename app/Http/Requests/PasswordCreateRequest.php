62469e76014a9b0238575a42736be0b38a67f03a
wuruiwm <wuruiwm@qq.com>
2020-07-12 16:00:55 +0800

    去除密码管理 提交时 用户名的必填

--- app/Http/Requests/PasswordCreateRequest.php
+++ app/Http/Requests/PasswordCreateRequest.php
@@ -7,7 +7,7 @@ class PasswordCreateRequest extends BaseRequest
     public function rules(){
         return [
             'title' => 'required|max:50',
-            'user' => 'required|max:50',
+            'user' => 'max:50',
             'password' => 'required|max:50',
         ];
     }
@@ -15,7 +15,6 @@ class PasswordCreateRequest extends BaseRequest
         return [
             'title.required'=>'请输入名称',
             'title.max'=>'名称最大为50字符',
-            'user.required'=>'请输入用户名',
             'user.max'=>'用户名最大为50字符',
             'password.required'=>'请输入密码',
             'password.max'=>'密码最大为50字符',
