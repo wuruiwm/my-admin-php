78b4c6d5c82ae794855b90bee5644b5a820e16a7
wuruiwm <wuruiwm@qq.com>
2020-07-12 16:29:27 +0800

    将获取ssl证书到期时间 生成临时文件路径 修改为/public/下

--- app/Models/Ssl.php
+++ app/Models/Ssl.php
@@ -10,9 +10,9 @@ class Ssl extends Base
         $data['pem'] = Cache::get("ssl_pem");
         $data['end_time'] = '';
         if(function_exists('exec') && !empty($data['pem'])){
-            file_put_contents(__DIR__.'/ssl_pem.txt',$data['pem']);
-            exec("openssl x509 -in ".__DIR__."/ssl_pem.txt"." -noout -text",$res);
-            unlink(__DIR__.'/ssl_pem.txt');
+            file_put_contents(public_path().'/ssl_pem.txt',$data['pem']);
+            exec("openssl x509 -in ".public_path()."/ssl_pem.txt"." -noout -text",$res);
+            unlink(public_path().'/ssl_pem.txt');
             foreach ($res as $k =>$v){
                 if(strpos($v,'Not After') !== false){
                     $tmp = strtotime(str_replace('Not After : ','',$v));
