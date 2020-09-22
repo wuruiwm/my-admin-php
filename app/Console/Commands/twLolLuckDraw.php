a95c8f1a0378612c4644ddd213e2f78b8c21151b
wurui <wuruiwm@qq.com>
2020-09-22 17:36:25 +0800

    台服lol幸运抽奖

---
+++ app/Console/Commands/twLolLuckDraw.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class twLolLuckDraw extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'twLolLuckDraw';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '台服lol幸运抽奖';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $lucky_draw_url = 'https://luckydraw.gamehub.garena.tw/service/luckydraw';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $post_data = [
+            'game'=>'lol',
+            'sk'=>admin_config('tw_lol_luck_draw_sk'),
+            'region'=>'TW',
+            'version'=>1599462082,
+            'tid'=>time(),
+        ];
+        $result = curl_post($this->lucky_draw_url,$post_data);
+        $data = @json_decode($result,true);
+        if(empty($data)){
+            $this->error('请求错误');
+            return;
+        }
+        $this->info(json_encode($data));
+    }
+}
