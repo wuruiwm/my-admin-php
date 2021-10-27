18185cb5aebceb4511d1273b45512b7fe677f377
吴瑞 <wuruiwm@qq.com>
2021-10-27 01:34:32 +0000

    调试台服lol抽奖脚本

--- app/Console/Commands/twLolLuckDraw.php
+++ app/Console/Commands/twLolLuckDraw.php
@@ -125,7 +125,7 @@ class twLolLuckDraw extends Command
      */
     public function initVersion(){
         $url = 'https://luckydraw.gamehub.garena.tw/service/luckydraw/?sk='.$this->sk.'&region=TW&tid='.time();
-        var_dump(@file_get_contents($url),'https://luckydraw.gamehub.garena.tw/service/luckydraw/?sk='.$this->sk.'&region=TW&tid='.time());exit();
+        var_dump(curl_get_https($url),'https://luckydraw.gamehub.garena.tw/service/luckydraw/?sk='.$this->sk.'&region=TW&tid='.time());exit();
         $result = @json_decode(@file_get_contents($url),true);
         if(empty($result)){
             exit('请求获取抽奖版本号接口失败');
@@ -176,5 +176,17 @@ class twLolLuckDraw extends Command
         }else{
             Cache::increment($num_cache_key);
         }
+    }
+    public function curl_get_https($url){
+        $curl = curl_init(); // 启动一个CURL会话
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);  // 从证书中检查SSL加密算法是否存在
+        $tmpInfo = curl_exec($curl);     //返回api的json对象
+        //关闭URL请求
+        curl_close($curl);
+        return $tmpInfo;    //返回json对象
     }
 }
