f6a725f40bd3f26f5af6cf9e14d05d9ebbf5fc96
吴瑞 <wuruiwm@qq.com>
2022-01-06 12:52:04 +0000

    爬取一言，增加异常捕获和sleep

--- app/Console/Commands/hitokoto.php
+++ app/Console/Commands/hitokoto.php
@@ -44,23 +44,28 @@ class hitokoto extends Command
         $success = 0;
         $i = 0;
         while($i < $num){
-            $res = file_get_contents('https://v1.hitokoto.cn/');
-            $res = json_decode($res,true);
-            $data = [];
-            $data['content'] = $res['hitokoto'];
-            $data['id'] = $res['id'];
-            !empty($res['created_at']) || $res['created_at'] = time();
-            $data['created_at'] = date('Y-m-d H:i:s',$res['created_at']);
-            $data['updated_at'] = date('Y-m-d H:i:s');
-            $data['type'] = model::typeTransformation($res['type']);
-            try {
-                model::insert($data);
-                $success++;
-                $this->info("插入成功");
-            } catch (\Throwable $th) {
+            try{
+                $res = file_get_contents('https://v1.hitokoto.cn/');
+                $res = json_decode($res,true);
+                $data = [];
+                $data['content'] = $res['hitokoto'];
+                $data['id'] = $res['id'];
+                !empty($res['created_at']) || $res['created_at'] = time();
+                $data['created_at'] = date('Y-m-d H:i:s',$res['created_at']);
+                $data['updated_at'] = date('Y-m-d H:i:s');
+                $data['type'] = model::typeTransformation($res['type']);
+                try {
+                    model::insert($data);
+                    $success++;
+                    $this->info("插入成功");
+                } catch (\Throwable $th) {
+                    $this->error("插入失败");
+                } 
+                $i++;
+            } catch (\Exception $e) {
                 $this->error("插入失败");
             }
-            $i++;
+            sleep(1);
         }
         send_email('一言爬虫','有效插入'.$success.'条');
         $this->info('有效插入'.$success.'条');
